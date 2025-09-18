80cddeb314bd5592994a2bddf20a6f68d236f6dd
chalamalasettydola <chalamalasettydola08@gmail.com>
2025-09-18 10:08:47 +0530

    Add files via upload

---
+++ primitive data types.py
@@ -0,0 +1,7 @@
+#write a python program to print the primitive data type using int/float/boolean/double
+a=10
+b=4.1
+c=True
+print("Integerr is: ",a)
+print("float is: ",b)
+print("boolean is: ",c)
