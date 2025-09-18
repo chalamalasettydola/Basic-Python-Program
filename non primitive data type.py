1259da4278a0428175844c1aac8906b88b757d82
chalamalasettydola <chalamalasettydola08@gmail.com>
2025-09-18 10:36:02 +0530

    Update non primitive data type.py

--- non primitive data type.py
+++ non primitive data type.py
@@ -1,8 +1,9 @@
 #write a python to print non primitive data type
 a=(10,20,80)
 b=[5,4,7,9,13]
-c{50,40,30}
+c={50,40,30}
 dict_1={"one":1,"two":2,"three":3}
 print(a)
 print(b)
 print(c)
+print(dict_1)
