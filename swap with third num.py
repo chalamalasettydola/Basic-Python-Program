a145ac7331056e74b48f3bc6162333a4c354f418
chalamalasettydola <chalamalasettydola08@gmail.com>
2025-09-18 10:01:43 +0530

    Add files via upload

---
+++ swap with third num.py
@@ -0,0 +1,9 @@
+#Swap the two numbers useing third variable
+a=int(input("Enter a value: "))
+b=int(input("Enter a value: "))
+temp=a
+a=b
+b=temp
+
+print("after swapping a: ",a)
+print("after swapping b: ",b)
