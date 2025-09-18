d481ac0dd156c61405dd582ec710ee27df7ea443
chalamalasettydola <chalamalasettydola08@gmail.com>
2025-09-18 11:41:09 +0530

    Add files via upload

---
+++ find largest number.py
@@ -0,0 +1,6 @@
+#To find the largest numbers
+num1=int(input("Enter the number 1: "))
+num2=int(input("Enter the number 2: "))
+num3=int(input("Enter the number 3: "))
+largest = max(num1,num2,num3)
+print("The largest number is: ",largest)
