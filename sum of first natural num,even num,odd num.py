3852b24c896bcb84e814270a0ddde9fc9d3d0467
chalamalasettydola <chalamalasettydola08@gmail.com>
2025-09-18 11:51:23 +0530

    Add files via upload

---
+++ sum of first natural num,even num,odd num.py
@@ -0,0 +1,8 @@
+#Sum of the first natural numbers,even numbers,odd numbers
+a=int(input("Enter the number : "))
+sum_natural = a*(a + 1)// 2
+sum_even = a*(a + 1)
+sum_odd = a*a
+print("Sum of first", a, "natural numbers:", sum_natural)
+print("Sum of first", a, "even numbers:", sum_even)
+print("Sum of first", a, "odd numbers:", sum_odd)
