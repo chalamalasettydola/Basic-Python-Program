8ce0ab8fc1cec7de72fa00ca3cd0f740f1eb78f7
chalamalasettydola <chalamalasettydola08@gmail.com>
2025-09-18 11:32:57 +0530

    Add files via upload

---
+++ multiply two floating points .py
@@ -0,0 +1,5 @@
+#write a python program to  multiply two floating points
+num1=float(input("Enter the number: "))
+num2=float(input("Enter the number: "))
+multiply=num1*num2
+print("the result is: ",multiply)
