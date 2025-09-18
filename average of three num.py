1487ebd3553c2a09d6762b891b1aa755a75ad77e
chalamalasettydola <chalamalasettydola08@gmail.com>
2025-09-18 11:00:43 +0530

    Add files via upload

---
+++ average of three num.py
@@ -0,0 +1,6 @@
+#Find the average of three number and we get the input from the user
+x=int(input("Enter the number: "))
+y=int(input("Enter the number: "))
+z=int(input("Enter the number: "))
+avg=x+y+z/3
+print("average of three numbers: ",avg)
